377de3d8a65dea50d451f8b5b528f94a57828a3f
Keel <keel.sike@gmail.com>
2012-04-12 17:09:54 +0800

    修改使用样例

--- src/cn/game189/test/SMSTest.java
+++ src/cn/game189/test/SMSTest.java
@@ -17,19 +17,22 @@ import android.widget.LinearLayout;
 import android.widget.Toast;
 
 /**
- * 短代代码使用样例,解决了以下问题：
- * 单机游戏置换存档破解;
- * 修改短信中心破解;
- * 无卡或废卡破解;
- * 双卡双待手机使用移动卡破解;
- * 过快点击造成重复计费;
+ * 爱游戏Android短代代码使用样例,解决了以下问题：
+ * 
+ * 防止过快点击造成重复计费;
  * 内存不足或机器状态不对导致无法存档造成重复计费;
- * 注意:使用最低需要android1.6,即<uses-sdk android:minSdkVersion="4" />
+ * 覆盖升级安装能保留原已计过费的计费点(卸载后会清除);
+ * 防止单机游戏置换存档破解;
+ * 防止修改短信中心破解;
+ * 防止无卡或废卡破解;
+ * 防止双卡双待手机使用移动卡破解;
+ * 注意:使用最低需要Android1.6t系统,即<uses-sdk android:minSdkVersion="4" />
  * 
  * 
  * <pre>
  * 使用方法：
- * 1.在AndroidManifest.xml中添加: 
+ * 1.引入sms.jar包;
+ * 2.在AndroidManifest.xml中添加: 
  * {@code 
  * <!-- android:screenOrientation指定是否横屏,删除即为自适应，
  * android:theme="@android:style/Theme.Dialog"
@@ -41,13 +44,13 @@ import android.widget.Toast;
  * <uses-permission android:name="android.permission.READ_PHONE_STATE" /> 
  * }
  * 
- * 2.创建一个或多个SMSListener处理不同的计费点发送结果;
- * 3.调用静态方法SMS.checkFee,注意feeName参数为计费点标识(不可含有#号),每个计费点必须不同
- * 4.可调用SMS.getResult()随时查看当前错误码.
+ * 3.创建一个或多个SMSListener处理不同的计费点发送结果;
+ * 4.调用静态方法SMS.checkFee,注意feeName参数为计费点标识(不可含有#号),每个计费点必须不同
+ * 5.可调用SMS.getResult()随时查看当前错误码.
  * </pre>
  * 
  * <pre>
- * 错误码:
+ * 错误码(可通过SMS.getResult()随时获取):
  * 0.初始值或取消发送
  * 1.已计过费
  * 2.发送短信成功
@@ -65,15 +68,13 @@ import android.widget.Toast;
  * </pre>
  * @author keel
  * 
+ * 
+ * 
+ * 
+ * 
  */
 public class SMSTest extends Activity implements SMSListener {
 
-	/**
-	 * 
-	 */
-	public SMSTest() {
-		
-	}
 	
 	private Button bt1;
 	
@@ -81,28 +82,38 @@ public class SMSTest extends Activity implements SMSListener {
 	
 	private Button bt3;
 	
+	private Button bt5;
+	
+	private String reLifeFeeName = "reLife";
+	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		LinearLayout layout = new LinearLayout(this);
+		layout.setOrientation(LinearLayout.VERTICAL);
 		setContentView(layout);
 		ViewGroup.LayoutParams ww = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT);
 		bt1 = new Button(this);
 		bt1.setLayoutParams(ww);
-		bt1.setText("计费点A");
+		bt1.setText("关卡A");
 		layout.addView(bt1);
 		
 		
 		bt2 = new Button(this);
 		bt2.setLayoutParams(ww);
-		bt2.setText("计费点B");
+		bt2.setText("关卡B");
 		layout.addView(bt2);
 		
 		bt3 = new Button(this);
 		bt3.setLayoutParams(ww);
-		bt3.setText("另一方式");
+		bt3.setText("原地复活");
 		layout.addView(bt3);
+		
+		bt5 = new Button(this);
+		bt5.setLayoutParams(ww);
+		bt5.setText("另一方式");
+		layout.addView(bt5);
 		
 		
 		
@@ -110,7 +121,6 @@ public class SMSTest extends Activity implements SMSListener {
 		//两个计费点测试按钮
 		bt1.setOnClickListener(new OnClickListener(){
 			public void onClick(View v) {
-				
 				checkFeeA();
 			}
         }); 
@@ -121,8 +131,19 @@ public class SMSTest extends Activity implements SMSListener {
 			}
 		});
 		
-		//跳转到第二种调用方式
 		bt3.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				//原地复活
+				//复活每次计费标识都不同,所以加上毫秒数区别
+				SMSTest.this.reLifeFeeName = SMSTest.this.reLifeFeeName+System.currentTimeMillis();
+				//不需要判断是否已计费
+				SMS.checkFee(SMSTest.this.reLifeFeeName, SMSTest.this, SMSTest.this, "0111C001741102210073711102210072550115174000000000000000000000000000", "使用“原地复活”功能,点击确定将会发送一条1元短信,不含信息费.", "发送成功!已成功复活!");
+			}
+		});
+		
+		
+		//跳转到第二种调用方式
+		bt5.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				Intent intent = new Intent();
 				intent.setClass(SMSTest.this, SMSTest2.class);
@@ -140,31 +161,41 @@ public class SMSTest extends Activity implements SMSListener {
 		String feeName = "mode_A";
 		
 		/*
-		 * 验证计费点,返回是否已成功计过费
-		 * @param feeName 计费点标识符，不可包含#号
+		 * 验证计费点核心静态方法,自动完成全部短代过程
+		 * @param feeName 计费点标识符,不可包含#号,不同计费点此值必须不同
 		 * @param activity Activity 不能为null
 		 * @param listener SMSListener接口,处理发送成功和失败的操作,不能为null
-		 * @param feeCode 短代代码串,请登录平台查询产品计费信息并完整复制对应的计费点!!费用按平台上此计费点的对应费用计!
+		 * @param feeCode 短代代码,请登录平台查询产品计费信息并完整复制对应的计费点!!费用按平台上此计费点的对应费用计!
 		 * @param tip 短代提示语
 		 * @param okInfo 短代发送成功的提示语
-		 * @return 是否计过费
+		 * @return 返回是否已计过费
 		 */
 		if (SMS.checkFee(feeName, this, this, "0111C001741102210071271102210070930115174000000000000000000000000000", "开启\"xxx-A\",点击确定将会发送一条1元短信,不含信息费.", "发送成功!已成功解锁!")) {
-			this.smsOK(feeName);
+			Toast.makeText(this, "已计过费，直接进入关卡"+feeName, Toast.LENGTH_SHORT).show();
 		}
 	}
 	
 	void checkFeeB() {
 		String feeName = "mode_B";
+		/*
+		 * 验证计费点核心静态方法,自动完成全部短代过程
+		 * @param feeName 计费点标识符,不可包含#号,不同计费点此值必须不同
+		 * @param activity Activity 不能为null
+		 * @param listener SMSListener接口,处理发送成功和失败的操作,不能为null
+		 * @param feeCode 短代代码,请登录平台查询产品计费信息并完整复制对应的计费点!!费用按平台上此计费点的对应费用计!
+		 * @param tip 短代提示语
+		 * @param okInfo 短代发送成功的提示语
+		 * @return 返回是否已计过费
+		 */
 		if (SMS.checkFee(feeName, this, this, "0211C001741102210071271102210070940115174000000000000000000000000000", "开启\"xxx-B\",点击确定将会发送一条2元短信,不含信息费.", "发送成功!已成功解锁!")) {
-			this.smsOK(feeName);
+			Toast.makeText(this, "已计过费，直接进入关卡"+feeName, Toast.LENGTH_SHORT).show();
 		}
 	}
 	
 	
 	/**
-	 * 已计费成功的处理
-	 * @param feeName 对应当前的SMS.STR_CHECK值
+	 * 在弹出计费提供框后，用户点击确定并计费成功后的处理
+	 * @param feeName 对应当计费标识值
 	 * @see cn.game189.sms.SMSListener#smsOK(java.lang.String)
 	 */
 	public void smsOK(String feeName) {
@@ -176,23 +207,38 @@ public class SMSTest extends Activity implements SMSListener {
 		}else if(feeName.equals("mode_B")){
 			//打开mode_B计费点后的操作
 			Toast.makeText(this, "第二个关卡"+feeName+"开启后的操作", Toast.LENGTH_SHORT).show();
+		}else if(feeName.equals(this.reLifeFeeName)){
+			//原地复活
+			Toast.makeText(this, "角色原地复活功能实现", Toast.LENGTH_SHORT).show();
 		}
 	}
 
 
 
 	/**
-	 * 发送失败,错误码含义如下:
+	 * 短代发送失败,错误码含义如下:
 	 * <pre>
-	 * 1 - 无卡
-	 * 2
-	 * 
+	 * 0.初始值或取消发送
+	 * 1.已计过费
+	 * 2.发送短信成功
+	 * -1.发送失败
+	 * -2.无卡
+	 * -3.非电信卡
+	 * -4.获取终端码失败
+	 * -5.保存计费点失败
+	 * -6.获取存档数据有误
+	 * -7.获取存档时读终端码失败
+	 * -8.获取存档时feeName不匹配
+	 * -9.获取存档时终端码不匹配
+	 * -10.获取存档发生异常 
+	 * -11.feeName不合法
 	 * </pre>
 	 * @param feeName 对应当前的SMS.STR_CHECK值
+	 * @param errorCode 错误码
 	 * @see cn.game189.sms.SMSListener#smsFail(int)
 	 */
 	public void smsFail(String feeName,int errorCode) {
-		Log.e("SMSListener", "计费失败!模式:"+feeName+" 错误码:"+errorCode);
+		Log.e("SMSListener", "计费失败!计费点:"+feeName+" 错误码:"+errorCode);
 		//其他错误处理操作,不给道具或不放行关卡
 	}
 
--- src/cn/game189/test/SMSTest2.java
+++ src/cn/game189/test/SMSTest2.java
@@ -27,35 +27,49 @@ public class SMSTest2 extends Activity {
 	private Button bt4;
 	private Button bt5;
 	
+	
+	
+	private Button bt7;
+	
+	private String reLifeFeeName = "reLife";
+	
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		LinearLayout layout = new LinearLayout(this);
+		layout.setOrientation(LinearLayout.VERTICAL);
 		setContentView(layout);
 		ViewGroup.LayoutParams ww = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT);
 		
 		bt3 = new Button(this);
 		bt3.setLayoutParams(ww);
-		bt3.setText("计费点C");
+		bt3.setText("关卡C");
 		layout.addView(bt3);
 		
 		bt4 = new Button(this);
 		bt4.setLayoutParams(ww);
-		bt4.setText("计费点D");
+		bt4.setText("关卡D");
 		layout.addView(bt4);
 		
 		bt5 = new Button(this);
 		bt5.setLayoutParams(ww);
-		bt5.setText("返回");
+		bt5.setText("原地复活");
 		layout.addView(bt5);
 		
+		bt7 = new Button(this);
+		bt7.setLayoutParams(ww);
+		bt7.setText("返回");
+		layout.addView(bt7);
 		
+		
+		//关卡C
 		bt3.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				String feeName = "mode_C";
 				/*
-				 * 验证计费点核心静态方法,自动完成全部短代过程,用法可样例源码
+				 * 验证计费点核心静态方法,自动完成全部短代过程
 				 * @param feeName 计费点标识符,不可包含#号,不同计费点此值必须不同
 				 * @param activity Activity 不能为null
 				 * @param listener SMSListener接口,处理发送成功和失败的操作,不能为null
@@ -64,39 +78,41 @@ public class SMSTest2 extends Activity {
 				 * @param okInfo 短代发送成功的提示语
 				 * @return 返回是否已计过费
 				 */
-				if (SMS.checkFee(
-						feeName,
-						SMSTest2.this,
-						new SMSListener() {
+				if (SMS.checkFee(feeName, SMSTest2.this,new SMSListener() {
+					
+					public void smsOK(String feeName) {
+						//短代发送成功
+						Log.i("SMSListener", "模式"+feeName+"已计费完成,关卡已打开.");
+						Toast.makeText(SMSTest2.this, "第二个模式关卡"+feeName+"开启后的操作", Toast.LENGTH_SHORT).show();
 
-							public void smsOK(String feeName) {
-								// 短代发送成功
-								Log.i("SMSListener", "模式" + feeName
-										+ "已计费完成,关卡已打开.");
-								Toast.makeText(SMSTest2.this,"第二个模式关卡" + feeName + "开启后的操作",Toast.LENGTH_SHORT).show();
-
-							}
-
-							public void smsFail(String feeName, int errorCode) {
-								// 短代发送失败,不给道具或不放行关卡
-								Log.e("SMSListener", "计费失败!模式:[" + feeName + "] 错误码:" + errorCode);
-							}
-						},
-						"0111C001741102210071271102210070930115174000000000000000000000000000",
-						"开启第二种方式" + feeName + ",点击确定将会发送一条1元短信,不含信息费.",
-						"发送成功!已成功解锁!")) {
-					// 已计过费
-					Log.i("SMSListener", "模式" + feeName + "已计费完成,关卡已打开.");
+					}
+					
+					public void smsFail(String feeName, int errorCode) {
+						//短代发送失败,不给道具或不放行关卡
+						Log.e("SMSListener", "计费失败!模式:["+feeName+"] 错误码:"+errorCode);
+					}
+				},  "0111C001741102210071271102210070930115174000000000000000000000000000", "开启第二种方式"+feeName+",点击确定将会发送一条1元短信,不含信息费.", "发送成功!已成功解锁!")) {
+					//已计过费
+					Toast.makeText(SMSTest2.this, "已计过费,关卡"+feeName+"开启后的操作", Toast.LENGTH_SHORT).show();
 				}
 			}
 		});
 		
 		
-		
+		//关卡D
 		bt4.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				String feeName = "mode_D";
-				//直接使用匿名SMSListener
+				/*
+				 * 验证计费点核心静态方法,自动完成全部短代过程
+				 * @param feeName 计费点标识符,不可包含#号,不同计费点此值必须不同
+				 * @param activity Activity 不能为null
+				 * @param listener SMSListener接口,处理发送成功和失败的操作,不能为null
+				 * @param feeCode 短代代码,请登录平台查询产品计费信息并完整复制对应的计费点!!费用按平台上此计费点的对应费用计!
+				 * @param tip 短代提示语
+				 * @param okInfo 短代发送成功的提示语
+				 * @return 返回是否已计过费
+				 */
 				if (SMS.checkFee(feeName, SMSTest2.this,new SMSListener() {
 					
 					public void smsOK(String feeName) {
@@ -115,8 +131,30 @@ public class SMSTest2 extends Activity {
 			}
 		});
 		
-		
+		//原地复活
 		bt5.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				//复活每次计费标识都不同,所以加上毫秒数区别
+				SMSTest2.this.reLifeFeeName = SMSTest2.this.reLifeFeeName+System.currentTimeMillis();
+				//直接使用匿名SMSListener,此处不需要判断是否已计过费
+				SMS.checkFee(SMSTest2.this.reLifeFeeName, SMSTest2.this,new SMSListener() {
+					
+					public void smsOK(String feeName) {
+						//短代发送成功
+						Toast.makeText(SMSTest2.this, "角色原地复活功能实现", Toast.LENGTH_SHORT).show();
+					}
+					
+					public void smsFail(String feeName, int errorCode) {
+						Log.e("SMSListener", "原地复活计费失败!模式:["+feeName+"] 错误码:"+errorCode);
+					}
+				}, "0111C001741102210073711102210072550115174000000000000000000000000000", "使用“原地复活”功能,点击确定将会发送一条1元短信,不含信息费.", "发送成功!已成功复活!");
+			}
+		});
+				
+		
+		
+		//返回
+		bt7.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				finish();
 			}
